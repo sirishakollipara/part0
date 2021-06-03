f45670ec0ccc9b8592ea9580006b3eaf81cbd9e2
sirishakollipara <61492679+sirishakollipara@users.noreply.github.com>
2021-06-03 18:29:04 +0530

    Update index.js

--- part2/part2-Phonebook/src/index.js
+++ part2/part2-Phonebook/src/index.js
@@ -4,16 +4,3 @@ import App from './app'
 import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
-/*
-const persons = [
-  { name: 'Arto Hellas', number: '040-123456' },
-  { name: 'Ada Lovelace', number: '39-44-5323523' },
-  { name: 'Dan Abramov', number: '12-43-234345' },
-  { name: 'Mary Poppendieck', number: '39-23-6423122' }
-]
-
-ReactDOM.render(
-  <App persons={persons} />,
-  document.getElementById('root')
-)*/
